eea0fd4b0f14f7cfd68f7a9a558c984c9bfb5eb1
Carlos Vera <kikiondo117@gmail.com>
2024-01-05 16:33:17 -0600

    Update og description

--- app/root.tsx
+++ app/root.tsx
@@ -22,15 +22,15 @@ export const meta: MetaFunction = () => [
   {
     charset: "utf-8",
     title: "KIKIS 404",
+    description: "Informacion de kikis 404, mercancia y agenda",
   },
   {
     name: "viewport",
     content: "width=device-width,initial-scale=1",
   },
   {
-    description: "Informacion de kikis 404, mercancia y agenda",
     "og:title": "KIKIS 404",
-    "og:description": "KIKIS 404, mercancia, agenda y más...",
+    "og:description": "Mercancia, agenda , DJ Sets y más..",
     "og:image": "https://kikis404.com/imgs/kikis_logo.JPEG",
     "og:url": "https://www.kikis404.com",
     "og:type": "website",
