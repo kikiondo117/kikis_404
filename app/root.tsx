d3db17f91ea75a0bb6798459dac9ff934f38d64e
Carlos Vera <kikiondo117@gmail.com>
2024-01-06 16:29:12 -0600

    Google analit

--- app/root.tsx
+++ app/root.tsx
@@ -12,6 +12,7 @@ import styles from "./tailwind.css";
 import globalStyles from "./global.css";
 import { BackButton } from "./components/common/BackButton/Backbutton";
 import { renderSegmentSnippet } from "./utils/segment";
+import { useEffect } from "react";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
@@ -61,11 +62,25 @@ export const meta: MetaFunction = () => [
 ];
 
 export default function App() {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-DCWKJY1429");
+  }, []);
+
   return (
     <html lang="en">
       <head>
         <Meta />
         <Links />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DCWKJY1429"
+        ></script>
       </head>
       <body
         className="bg-black text-white"
@@ -74,6 +89,8 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
+        <script dangerouslySetInnerHTML={{ __html: renderSegmentSnippet() }} />
+
         <LiveReload />
       </body>
     </html>
@@ -113,17 +130,6 @@ export function ErrorBoundary() {
         </main>
 
         <Scripts />
-
-        <script dangerouslySetInnerHTML={{ __html: renderSegmentSnippet() }} />
-        {/* <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","screen","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware","register"];analytics.factory=function(e){return function(){if(window.analytics.initialized)return window.analytics[e].apply(window.analytics,arguments);var i=Array.prototype.slice.call(arguments);if(["track","screen","alias","group","page","identify"].indexOf(e)>-1){var c=document.querySelector("link[rel='canonical']");i.push({__t:"bpc",c:c&&c.getAttribute("href")||void 0,p:location.pathname,u:location.href,s:location.search,t:document.title,r:document.referrer})}i.unshift(e);analytics.push(i);return analytics}};for(var i=0;i<analytics.methods.length;i++){var key=analytics.methods[i];analytics[key]=analytics.factory(key)}analytics.load=function(key,i){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=i};analytics._writeKey="8FRB6yEFpo8At9HTJBxwPod6WNFDD0EX";;analytics.SNIPPET_VERSION="5.2.0";
-          analytics.load("8FRB6yEFpo8At9HTJBxwPod6WNFDD0EX");
-          analytics.page();
-          }}();`,
-          }}
-        /> */}
       </body>
     </html>
   );
