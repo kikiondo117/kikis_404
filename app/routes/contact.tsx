4cf61873688249cc406b020e4f04cb7eda56d6d0
Carlos Vera <kikiondo117@gmail.com>
2024-01-06 14:50:03 -0600

    Add short video

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -4,7 +4,7 @@ import { Menu } from "~/components/common/Menu/Menu";
 export default function Contact() {
   return (
     <main
-      className="container mt-20  mx-auto text-white"
+      className="container mt-20  mx-auto text-white pb-8"
       style={{ lineHeight: "1.4" }}
     >
       <div className="mx-auto w-[300px] max-w-xl mt-4 md:mt-8 relative md:w-[80vw]">
